49e0307b5e9ad55980c5881675a4a9d6196965ca
MaximilianV <maximilian.voelker@mailbox.org>
2017-11-25 16:38:47 +0100

    introduce requests session in connector

--- thingconnector/thingconnector.py
+++ thingconnector/thingconnector.py
@@ -1,5 +1,6 @@
 import settings
 import requests
+import json
 
 BASE_URL = "https://things.apps.bosch-iot-cloud.com/api/2/"
 THINGS_URI = "things/{thingId}"
@@ -7,19 +8,29 @@ THINGS_URI = "things/{thingId}"
 
 class ThingConnector:
 
+    def __init__(self):
+        self.session = requests.Session()
+        self.session.auth = (settings.username, settings.password)
+        self.session.headers.update({'x-cr-api-token': settings.api_token,
+                                     'Content-Type': 'application/json'})
+
     def create_thing(self, thing_id):
         uri = THINGS_URI.format(thingId=thing_id)
-        self.put(uri)
+        data = {'attributes': {'type': 'thingberry'}}
+        return ThingConnector.get_response_message(self.put(uri, data), "thing")
 
-    def put(self, uri):
-        headers = {'x-cr-api-token': settings.api_token}
-        r = requests.get(ThingConnector.build_url(uri), headers, auth=(settings.username, settings.password))
-        print(r.json())
-
-    @staticmethod
-    def get_header():
-        return
+    def put(self, uri, data):
+        r = self.session.put(ThingConnector.build_url(uri), json=data)
+        return r
 
     @staticmethod
     def build_url(uri):
         return BASE_URL + uri
+
+    @staticmethod
+    def get_response_message(request, artifact_name):
+        if request.status_code == requests.codes.created:
+            return "Successfully created the " + artifact_name
+        if request.status_code == requests.codes.no_content:
+            return "Updated or deleted the " + artifact_name
+        return request.reason
