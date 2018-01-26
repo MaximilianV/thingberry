b3100a592b5379959f8d9709858e32b29447d0a1
MaximilianV <maximilian.voelker@mailbox.org>
2018-01-26 20:59:23 +0100

    add display component scaffolding

---
+++ component/components/display_component.py
@@ -0,0 +1,10 @@
+from component import ActionComponent
+
+
+class DisplayComponent(ActionComponent):
+    def trigger_action(self, **kwargs):
+        pass
+
+    @staticmethod
+    def configure_action():
+        pass
