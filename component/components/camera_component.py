0d4558068f40be36b22644e02c624560d210d998
MaximilianV <maximilian.voelker@mailbox.org>
2018-01-26 20:40:00 +0100

    implement camera component

--- component/components/camera_component.py
+++ component/components/camera_component.py
@@ -1,5 +1,31 @@
 from component import ActionComponent
+import logging
+from picamera import PiCamera
+from time import sleep
+import random
+import string
 
 
 class CameraComponent(ActionComponent):
-    pass
+    def trigger_action(self, **kwargs):
+        delay = int(self.action_config["delay"])
+        if delay == 0:
+            delay = 1
+        destination = self.action_config["destination"]
+        logging.debug("Taking a photo in " + str(delay) + " seconds.")
+        filename = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(32)])
+        filename += '.jpg'
+        camera = PiCamera()
+        sleep(delay)
+        camera.capture(destination + filename)
+        logging.debug("Photo taken and stored in " + str(destination) + ".")
+
+    @staticmethod
+    def configure_action():
+        print("Configuring Camera Action:")
+        delay = int(input("Delay until photo is taken (def. 2): "))
+        destination = input("\nDestination to save image: ")
+        return {
+            "delay": delay,
+            "destination": destination
+        }
