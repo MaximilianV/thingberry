938ee24467010a91b70cf78bfd89eb4527f46575
MaximilianV <maximilian.voelker@mailbox.org>
2018-01-26 21:01:33 +0100

    add thingID to component constructor

--- component/component.py
+++ component/component.py
@@ -3,7 +3,8 @@ import logging
 
 
 class Component:
-    def __init__(self, **kwargs):
+    def __init__(self, thing_id, **kwargs):
+        self.thing_id = thing_id
         self.connector = ThingConnector()
         self.logger = logging.getLogger(__name__)
         self.setup_logging()
