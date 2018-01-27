b1fe74c1a928b24605c9f214ef0550187f0021c3
MaximilianV <maximilian.voelker@mailbox.org>
2018-01-27 17:34:37 +0100

    add first display implementation

--- component/components/display_component.py
+++ component/components/display_component.py
@@ -1,10 +1,14 @@
 from component import ActionComponent
+from .lib.lcddriver import lcd as LCD
 
 
 class DisplayComponent(ActionComponent):
-    def trigger_action(self, **kwargs):
-        pass
+    def trigger_action(self, value, **kwargs):
+        lcd = LCD()
+        lcd.lcd_clear()
+        lcd.lcd_display_string(value, 1)
 
     @staticmethod
     def configure_action():
-        pass
+        print("The Display Action doesn't need to be configured.")
+        return {}
