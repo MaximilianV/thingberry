113b769b87d864027cbac0e5ca0c1c2d8eaac0e1
MaximilianV <maximilian.voelker@mailbox.org>
2018-01-27 11:43:57 +0100

    delete unused files

--- watch.py
+++
@@ -1,7 +0,0 @@
-from thing import Thing
-from thingconnector.thingwatcher import ThingWatcher
-
-thing = Thing()
-thing.load_settings()
-thingwatcher = ThingWatcher(thing)
-thingwatcher.start_watching()
