7b00d9bce90b2888354a8320b10299a422ad42cb
MaximilianV <maximilian.voelker@mailbox.org>
2018-01-27 15:17:00 +0100

    fix GPIO listening

--- component/components/button_component.py
+++ component/components/button_component.py
@@ -1,23 +1,26 @@
 from component import ObserverComponent
 import logging
 import RPi.GPIO as GPIO
+import time
 
 
 class ButtonComponent(ObserverComponent):
     def start_observe(self, **kwargs):
+        channel = int(self.property_config["pin"])
         GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(int(self.property_config["pin"]), GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.setup(channel, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         GPIO.add_event_detect(int(self.property_config["pin"]), GPIO.RISING)
-        super().start_observe(**kwargs)
+        logging.info("Add event detection on channel " + str(channel))
+        super().start_observe(channel=channel)
 
-    def observe(self, initial_count=0, **kwargs):
+    def observe(self, channel, initial_count=0):
         count = initial_count
         while True:
-            channel = GPIO.wait_for_edge(int(self.property_config["pin"]), GPIO.RISING)
-            if channel:
-                logging.debug("Detected button press for: " + self.property_name)
+            if GPIO.event_detected(channel):
+                logging.info("Detected button press for: " + self.property_name)
                 count += 1
                 self.update_property(count)
+            time.sleep(0.25)
 
     @staticmethod
     def configure_observer():
