0e53c991b1fa99be9dd3810c98793b07c4c300f8
MaximilianV <maximilian.voelker@mailbox.org>
2018-01-27 18:03:05 +0100

    finish basic display component

--- component/components/display_component.py
+++ component/components/display_component.py
@@ -1,12 +1,22 @@
 from component import ActionComponent
 from .lib.lcddriver import lcd as LCD
+import math
+import logging
 
 
 class DisplayComponent(ActionComponent):
     def trigger_action(self, value, **kwargs):
         lcd = LCD()
         lcd.lcd_clear()
-        lcd.lcd_display_string(value, 1)
+        lines = value.split("\n")
+        short_lines = []
+        for i in range(0, len(lines)):
+            for ii in range(0, int(math.ceil(len(lines[i])/16))):
+                short_lines.append(lines[i][ii*16:(ii+1)*16])
+        logging.info("Displaying:", end="")
+        logging.info(short_lines)
+        for line_number, line_content in enumerate(short_lines):
+            lcd.lcd_display_string(line_content, line_number + 1)
 
     @staticmethod
     def configure_action():
