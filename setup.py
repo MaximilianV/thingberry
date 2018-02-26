5cbea4ef9710ca9bd1c13ef7fa1ffea7133d3302
MaximilianV <maximilian.voelker@mailbox.org>
2018-02-26 16:14:47 +0100

    add method documentation for setup and utils (reStructuredText format)

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def request_thing_name():
     Asks the user for a thing name. The name has to consist of URI
     allowed characters only (http://www.ietf.org/rfc/rfc2396.txt).
     :return: uri conform string
-    :rtype: basestring
+    :rtype: str
     """
     raw_name = input("Please choose a name for your new thing:\n")
     return utils.input_require_match_reqex(THING_NAME_REGEX, raw_name)
@@ -25,14 +25,13 @@ def request_thing_name():
 
 def setup_artifact(artifact, parent_artifact=None):
     """
-    Requests a name for the new artifact to be created and in case it is a new feature, sub-properties will be
-    configurable too.
-    :param artifact: the artifact type that should be added
-    :type artifact: ThingArtifact
-    :param parent_artifact: default "none", name of parent feature if properties are created
-    :type parent_artifact: basestring
-    :return: None
-    :rtype: None
+    Lets the user setup (name) a new artifact (an artifact can be anything from the ThingArtifact-enum defined in utils).
+    :param artifact: kind of artifact to setup
+    :type artifact: a ThingArtifact-enum element
+    :param parent_artifact: name of the artifact of higher order
+    :type parent_artifact: str
+    :return: name of the configured artifact
+    :rtype: str
     """
     name = input("Please name the new " + artifact.name + ":\n")
     name = utils.input_require_match_reqex(FEATURE_PROPERTY_REGEX, name)
@@ -41,6 +40,12 @@ def setup_artifact(artifact, parent_artifact=None):
 
 
 def choose_feature():
+    """
+    Offers the user a list of feature-names so he can choose one.
+    The user can also add a new feature and select this feature, instead of an already existing one.
+    :return: name of the selected feature
+    :rtype: str
+    """
     features = thing.get_list_of_features()
     print("Please select a feature to insert the component:")
     feature_id = utils.ask_choose_index_from_list_or_new(features)
@@ -49,27 +54,29 @@ def choose_feature():
     return features[feature_id]
 
 
-def choose_property(feature_name):
-    return setup_artifact(ThingArtifact.Property, feature_name)
-
-
 def setup_component():
+    """
+    Shows a list of available components to the user and lets the user choose and configure one component.
+    :return: None
+    :rtype: None
+    """
     print("Please choose a component to be added to your thing:")
     component = utils.ask_choose_from_enum(Components)
-    # Try to execute config method for observer
+
+    # Each component can be an action, observer or even both.
+    # As we can not be sure which combination it is, we use the pythonic way of exceptions in the following.
+    # Try to execute config method for observer-component
     try:
         config_method = component.value.configure_observer
     except AttributeError:
         pass
     else:
         feature_name = choose_feature()
-        property_name = choose_property(feature_name)
+        property_name = setup_artifact(ThingArtifact.Property, feature_name)
         property_config = config_method()
         property_config["type"] = component.name
         thing.set_property_config(feature_name, property_name, property_config)
-
-
-    # Try to execute config method for action
+    # Try to execute config method for action-component
     try:
         config_method = component.value.configure_action
     except AttributeError:
@@ -86,8 +93,7 @@ def setup_component():
 def setup_new_thing():
     """
     Starts the setup procedure for a new thing.
-    :param thing: the thing object to be setup
-    :type thing: Thing
+    The Thing that is configured, is saved as a module global variable.
     :return: None
     :rtype: None
     """
@@ -103,6 +109,8 @@ def setup_new_thing():
 def main():
     """
     This methods leads through the whole setup process for a thing.
+    After setup, the user is asked whether the Thing should be instantiated in the cloud and whether
+    the configuration should be saved to disk (necessary for running the Thing).
     :return: None
     :rtype: None
     """
--- utils.py
+++ utils.py
@@ -1,7 +1,16 @@
 import re
 from enum import Enum
 
+
 def ask_yes_no_question(question):
+    """
+    Asks the user to answer the given question with yes or no.
+    Yes, Y, No and N are accepted as valid responses (case insensitive).
+    :param question: the question to be answered
+    :type question: str
+    :return: the user's answer
+    :rtype: bool
+    """
     do_it = input(question + " (y/n)\n").lower()
     while not re.match("^([yjn]|yes|no)$", do_it):
         do_it = input("Please type y(es) or n(o):\n")
@@ -11,6 +20,13 @@ def ask_yes_no_question(question):
 
 
 def ask_choose_from_enum(enum):
+    """
+    Asks the user to choose an element of a given enum.
+    :param enum: the enum to choose from
+    :type enum: enumeration
+    :return: the chosen enum element
+    :rtype: enumeration member
+    """
     i = 0
     entries = []
     for entry in enum:
@@ -22,6 +38,13 @@ def ask_choose_from_enum(enum):
 
 
 def ask_choose_index_from_list_or_new(a_list):
+    """
+    Asks the user to choose an element from a given list or whether the user wants to create a new entry.
+    :param a_list: the list to choose from
+    :type a_list: list
+    :return: the chosen index (if "new entry" is selected, return None)
+    :rtype: int, None
+    """
     print("1\tNew entry")
     i = 1
     for entry in a_list:
@@ -34,12 +57,30 @@ def ask_choose_index_from_list_or_new(a_list):
 
 
 def input_require_match_reqex(regex, raw_input):
+    """
+    Utility function that requires a given input to match a given regular expression.
+    In case the expression is not met, ask the user to repeat the input in a valid format.
+    :param regex: the regular expression the input should be checked with
+    :type regex: str
+    :param raw_input: the initial input to check
+    :type raw_input: str
+    :return: the user input matching the given expression
+    :rtype: str
+    """
     while not re.match("^[_a-zA-Z][_a-zA-Z0-9\-]*$", raw_input):
         raw_input = input("Please input a valid string as name (should match \"" + regex + "\"):\n")
     return raw_input
 
 
 def input_require_int(raw_input):
+    """
+    Utility function that requires a given input to be an integer greater zero.
+    In case the given input is not an integer or zero or negative, the user is requested to provide a valid input.
+    :param raw_input: the initial user input
+    :type raw_input: str
+    :return: a number greater zero the user inserted
+    :rtype: int
+    """
     while True:
         try:
             if int(raw_input) > 0:
@@ -51,6 +92,10 @@ def input_require_int(raw_input):
 
 
 class ThingArtifact(Enum):
+    """
+    This enum defines what different kind of artifacts a Thing can have and is used during setup process to
+    simplify the code.
+    """
     Attribute = 1
     Feature = 2
     Property = 3
